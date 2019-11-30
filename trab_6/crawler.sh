34f9abac7c3215bd0a5d1ae63fa4233b576f4031
kalilcazes <kalilcazes@gmail.com>
2019-11-30 05:00:12 -0300

    Adicionando hora inicial; Corrigindo logica de comparacao e de escrita

--- trab_6/crawler.sh
+++ trab_6/crawler.sh
@@ -5,28 +5,27 @@ i=0
 time=$(date +%M)
 
 while true; do
+    initial_time=$(date +%H:%M)
     while [[ "${time}" -eq $(date +%M) ]]; do
         price[i]+=$( curl -Ss  https://br.advfn.com/bolsa-de-valores/bmf/WINZ19/cotacao | grep quoteElementPiece19 | awk '{print $NF}' | sed 's/[Aa-zZ>"=&;.,</]//g' | sed 's/.\{2\}$//' ) 
         max_price=${price[$i]}
         min_price=${price[$i]}
 
-        if (( "${max_price}" > "${price[$i]}" )); then
+        if (( "${max_price}" < "${price[$i]}" )); then
             max_price="${price[$i]}"
         fi
 
-        if (( "${min_price}" < "${price[$i]}" )); then
+        if (( "${min_price}" > "${price[$i]}" )); then
             min_price="${price[$i]}"
         fi
 
-        echo "${price[$i]}"
         ((i++))    
     done;
     
     first_price=${price[0]}
     last_price=${price[-1]}
 
-    for(( i=0; "${i}"<"${#price[@]}"; i++ )); do
-        echo -n "${first_price},${last_price},${max_price},${min_price}," >> data
-    done
+    echo -n "${initial_time},${first_price},${max_price},${min_price},${last_price}," >> data
+
 
 done
