8a8215df79a0f2588eaeb680bec127a01d57b60f
kalilcazes <kalilcazes@gmail.com>
2019-12-01 23:00:49 -0300

    Adicionando logica para cricao de arquivo

--- trab_6/price.sh
+++ trab_6/price.sh
@@ -1,9 +1,25 @@
+#!/bin/bash
+
 cleanup() {
     pkill -P $$
     exit 0
 }
 trap cleanup SIGHUP SIGINT SIGTERM
 
-sh crawler.sh & 
-sh candlestick.sh
+echo "Type file name to plot old data or press [RETURN] to collect new data:"
+
+read -r INPUT
+if [[ "${INPUT}" = "" ]]; then 
+    file_name="./data/data_$(date +%H:%M:%S).fk"
+    ./crawler.sh "${file_name}" & 
+    ./candlestick.sh "${file_name}"
+
+elif [[ -f "${INPUT}" && "${INPUT: -3}" == ".fk" ]]; then 
+    file_name="${INPUT}"
+    ./candlestick.sh "${file_name}"
+else
+    echo "File not found or not compatible."
+    exit 1
+fi
+
 
